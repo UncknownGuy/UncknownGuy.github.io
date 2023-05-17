f793fe1e01b0e5c9912360f5198ced42b9ff0f55
UncknownGuy <130330950+UncknownGuy@users.noreply.github.com>
2023-05-17 06:01:54 -0700

    💠▫️𝖵1 𝗗𝗘𝗠𝗢 𝗟𝗔𝗦𝗧𝗘𝗦𝗧 🚨💠
    * 𝗠𝗘𝗡𝗨 𝗔𝗗𝗗𝗘𝗗 𝗠𝗘𝗡𝗨 𝗖𝗦𝗦 𝗔𝗗𝗗𝗘𝗗 𝗝𝗔𝗩𝗔𝗦𝗖𝗥𝗜𝗣𝗧 𝗔𝗗𝗗𝗘𝗗

---
+++ menu.js
@@ -0,0 +1,13 @@
+function menuButton(x) {
+    x.classList.toggle("change");
+}
+
+function toggleNav() {
+    var x = document.getElementById("myNav");
+    if (x.style.height === "100%") {
+    x.style.height = "0%";
+    } 
+    else {
+    x.style.height = "100%";
+    }
+}
