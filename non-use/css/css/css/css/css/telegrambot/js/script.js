d5d449dec6e1e015612a639f83958b40cb0a7a0c
UncknownGuy <cocmaster748596@gmail.com>
2024-12-05 13:42:21 +0530

    Delete non-use directory

--- non-use/css/css/css/css/css/telegrambot/js/script.js
+++
@@ -1,8 +0,0 @@
-// Function to refresh the web page
-function refreshPage() {
-  console.log('Refreshing page...');
-  location.reload();
-}
-
-// Set the timeout to refresh after 1 hour
-setTimeout(refreshPage, 60 * 60 * 1000);
